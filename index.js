db75daff43e8603018d9e1c73dce9b38b5c13732
Ian Hansen <ian@supershabam.com>
2013-11-19 22:42:34 -0700

    first

---
+++ index.js
@@ -0,0 +1,30 @@
+var URL = require('url')
+
+exports.parse = function(urlStr) {
+  var url    = URL.parse(urlStr)
+    , secure = false
+
+  if (url.protocol !== 'ws' && url.protocol !== 'wss') {
+    throw new Error('scheme component must be "ws" or "wss"')
+  }
+  if (url.hash.length) {
+    throw new Error('expected null fragment component')
+  }
+  if (url.protocol === 'wss') {
+    secure = true
+  }
+  if (!url.port) {
+    if (url.protocol === 'ws') {
+      url.port = '80'
+    } else {
+      url.port = '443'
+    }
+  }
+  return {
+    host     : url.hostname,
+    port     : url.port,
+    resource : url.path,
+    secure   : secure
+  }
+}
+
